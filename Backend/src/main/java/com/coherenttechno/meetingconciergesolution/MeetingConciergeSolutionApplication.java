93c4503f65acabf8ab117557f52cc887039febda
Narayan Ram <iamnarayanramn2r@gmail.com>
2020-06-28 13:12:56 +0530

    Initial Commit Structure

---
+++ Backend/src/main/java/com/coherenttechno/meetingconciergesolution/MeetingConciergeSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.coherenttechno.meetingconciergesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingConciergeSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingConciergeSolutionApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/coherenttechno/meetingconciergesolution/MeetingConciergeSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coherenttechno.meetingconciergesolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingConciergeSolutionApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
