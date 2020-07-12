b5957d164df18e48e13a2074655f42c6a95d6c32
Narayan Ram <iamnarayanramn2r@gmail.com>
2020-07-12 15:40:30 +0530

    update src

--- Backend/src/main/java/com/coherenttechno/meetingconciergesolution/MeetingConciergeSolutionApplication.java
+++ Backend/src/main/java/com/coherenttechno/meetingconciergesolution/MeetingConciergeSolutionApplication.java
@@ -2,8 +2,16 @@ package com.coherenttechno.meetingconciergesolution;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.coherenttechno.meetingconciergesolution.client", "com.coherenttechno.meetingconciergesolution.repository",
+        "com.coherenttechno.meetingconciergesolution.service", "com.coherenttechno.meetingconciergesolution.controller",
+        "com.coherenttechno.meetingconciergesolution.exception", "com.coherenttechno.meetingconciergesolution.config"})
+@EntityScan(basePackages = {"com.coherenttechno.meetingconciergesolution.repository.model"})
+@EnableJpaRepositories(basePackages = {"com.coherenttechno.meetingconciergesolution.repository.dao"})
 public class MeetingConciergeSolutionApplication {
 
     public static void main(String[] args) {
